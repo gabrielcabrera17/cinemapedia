5cb30215b7d6affd985deb0a8fd0ff1a7c1a211f
Gabriel <gabrielbaez023@gmail.com>
2024-11-16 16:18:34 -0300

    MovieSlideShow - Carrusel de películas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -40,18 +40,10 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   
     return Column(
       children: [
-        CustomAppbar(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          ),
-        )
+        
+        const CustomAppbar(),
+
+        MoviesSlidesShow(movies: nowPlayingMovies)
 
       ],
     );
