f19fbf8713743b3be5156af582cb54d8e40eaf68
Gabriel <gabrielbaez023@gmail.com>
2025-04-24 21:09:39 -0300

    Añadiendo rutas hijas, para mejorar la navegación web

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -8,15 +8,19 @@ final appRouter = GoRouter(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) =>  const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state){
-        final movieId = state.pathParameters['id'] ?? 'no-id';
-        
-        return  MovieScreen(movieId: movieId,);
-      }
-    )
-  ]
-);
+      //rutas hijas
+      routes: [
+         GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state){
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                
+                return  MovieScreen(movieId: movieId,);
+              }
+            )
+          ]
+        )
+      ]
+    );
+   
