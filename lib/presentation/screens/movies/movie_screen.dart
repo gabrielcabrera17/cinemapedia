1fae195cc75fd03387c7e98db5c9d209fea9421c
Gabriel <gabrielbaez023@gmail.com>
2025-08-15 20:30:19 -0300

    vista de los actores

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -106,7 +106,8 @@ class _MovieDetails extends StatelessWidget {
         ),
 
         // TODO: mostrar actores ListView
-        const SizedBox(height: 80,)
+        _ActosByMovie(movieId: movie.id.toString(),),
+        const SizedBox(height: 50,)
       ],
     );
   }
@@ -128,10 +129,10 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(movie.title, 
-        style: const TextStyle(color: Colors.white, fontSize: 20),
-        textAlign: TextAlign.start,
-        ),
+        //title: Text(movie.title, 
+        //style: const TextStyle(color: Colors.white, fontSize: 20),
+        //textAlign: TextAlign.start,
+        //),
         background: Stack(
           children: [
             SizedBox.expand(
@@ -171,6 +172,58 @@ class _CustomSliverAppBar extends StatelessWidget {
         ),
       ),
       
+    );
+  }
+}
+
+class _ActosByMovie extends ConsumerWidget {
+
+  final String movieId;
+  const _ActosByMovie({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if(actorsByMovie[movieId] == null){
+      return CircularProgressIndicator(strokeWidth: 2);
+    }
+
+    final actors = actorsByMovie[movieId];
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors!.length,
+        itemBuilder:(context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Actor Photo
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(28),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 5,),
+                Text(actor.name ?? 'no especifícado', maxLines: 2,),
+                Text(actor.character ?? '', maxLines: 2, style: TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis))
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
