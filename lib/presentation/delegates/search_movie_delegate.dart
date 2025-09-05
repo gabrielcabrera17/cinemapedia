4e3164c715bb1169dc5cbe7bc0b715d4d637b69d
Gabriel <gabrielbaez023@gmail.com>
2025-09-05 20:46:42 -0300

    customizando Leading y Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,32 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
-
-class SearchMovieDelegate extends SearchDelegate{
+//Especificamos que queremos regresar una Movie
+class SearchMovieDelegate extends SearchDelegate<Movie?>{
   @override  
   String get searchFieldLabel => 'Buscar Pelicula';
   //build para construir acciones, iconos, etc.
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text('BuildActions')
+      //if(query.isNotEmpty)
+      //al presionar el botón se le asigna al query un string vacio
+      FadeIn(
+        //utilizamos animatedo de fer herrera para hacer desparecer
+        // el botón de clear con más estilo
+        animate: query.isNotEmpty,
+        child: IconButton(onPressed: () => query = '', 
+        icon: const Icon(Icons.clear)
+        ),
+      )
+
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(context, null), icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
