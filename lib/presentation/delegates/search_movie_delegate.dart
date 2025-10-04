9a79df120aeca8109b2586a34fef6eacb7b6fb6e
Gabriel <gabrielbaez023@gmail.com>
2025-10-04 09:55:44 -0300

    Mostrar calificación de la película

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 //funciono que mandare llamar para consultar mis peliculas 
@@ -85,7 +86,7 @@ class _MovieItem extends StatelessWidget {
           SizedBox(
             width: size.width*0.2,
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(10),
               child: Image.network(
                 movie.posterPath,
                 loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
@@ -104,7 +105,15 @@ class _MovieItem extends StatelessWidget {
                   Text(movie.title, style: textStyles.titleMedium,),
                   movie.overview.length > 100
                    ? Text('${movie.overview.substring(0,100)}...')
-                   : Text(movie.overview)
+                   : Text(movie.overview),
+
+                   Row(
+                    children: [
+                      Icon(Icons.star_half_rounded, color: Colors.yellow.shade800),
+                      SizedBox(width: 5,),
+                      Text(HumanFormat.number(movie.voteAverage,1),style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),)
+                    ],
+                  )
               ],
             ),
           )
