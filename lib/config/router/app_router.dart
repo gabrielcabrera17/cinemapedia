c340d9042e1f571e2cdb42ced497b42f28dea18a
Gabriel <gabrielbaez023@gmail.com>
2025-12-22 16:14:40 -0300

    Creación de Vistas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,49 +1,13 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
-import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-
-    ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
-      },
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) {
-            return const HomeView();
-          },
-          routes: [
-             GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state){
-                final movieId = state.pathParameters['id'] ?? 'no-id';
-                
-                return  MovieScreen(movieId: movieId,);
-              }
-            )
-          ]
-        ),
-
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) {
-            return const FavoritesView();
-          },
-        )
-      ],
-    )
-
-
-    //Rutas padre/hijo
-    /*GoRoute(
+    GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) =>  const HomeScreen(childView: HomeView(),),
+      builder: (context, state) =>  const HomeScreen(),
       //rutas hijas
       routes: [
          GoRoute(
@@ -56,9 +20,7 @@ final appRouter = GoRouter(
               }
             )
           ]
-        )*/
-
-
+        )
       ]
     );
    
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,5 @@
+
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/widget/widget.dart';
 import 'package:flutter/material.dart';
 
@@ -5,18 +7,13 @@ class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
 
-  final Widget childView;
-
-  const HomeScreen({
-    super.key, 
-    required this.childView
-  });
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:childView,
-      bottomNavigationBar: const CustomBottomNavigation(),
+    return  const Scaffold(
+      body: HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
 
     );
   }
