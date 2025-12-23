b01e83a0fb212b524668226d06e2544b014b6adc
Gabriel <gabrielbaez023@gmail.com>
2025-12-23 16:44:01 -0300

    Resolviendo la navegación

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -9,7 +9,7 @@ final appRouter = GoRouter(
       name: HomeScreen.name,
       builder: (context, state) {
         final pageIndex = state.pathParameters['page'] ?? '0';
-        print('page index $pageIndex');
+       
         return  HomeScreen(pageIndex: int.parse(pageIndex),);
       },
       //rutas hijas
@@ -24,6 +24,11 @@ final appRouter = GoRouter(
               }
             )
           ]
+        ),
+
+        GoRoute(
+          path: '/',
+          redirect: (_, __) => '/home/0',
         )
       ]
     );
